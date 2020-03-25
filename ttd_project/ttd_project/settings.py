5ca01028bf9de6d7652a2c2c50d5365d3ddd9126
unknown <kosinskiy.andrey@ukr.net>
2020-03-25 15:40:30 +0200

    added frontend

--- ttd_project/ttd_project/settings.py
+++ ttd_project/ttd_project/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
 
     'todo.apps.TodoConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
